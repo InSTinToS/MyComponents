f14d091e75b2e0493705b4bc069762230b168a6d
Miguel <miguelandradebarreto2@gmail.com>
2022-12-06 13:45:53 -0300

    :bug: has no exported

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
-export * from './components/SimpleSlider'
-export * from "./components/SimpleSlider/types"
+import { SimpleSlider } from './components/SimpleSlider'
+import { ISimpleSliderProps } from './components/SimpleSlider//types'
+
+export { ISimpleSliderProps, SimpleSlider }
